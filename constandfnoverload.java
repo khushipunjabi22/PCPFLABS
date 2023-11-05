d29ef516f2154366154351b4aa0d5f17fef4aea2
khushipunjabi22 <128999136+khushipunjabi22@users.noreply.github.com>
2023-11-05 20:34:01 +0530

    Update constandfnoverload.java

--- constandfnoverload.java
+++ constandfnoverload.java
@@ -1,45 +1,28 @@
-class Shape {
-}
-
-class Rectangle extends Shape {
+class Rectangle {
     private double length;
     private double width;
 
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
+    public Rectangle(double l, double w) {
+        length = l;
+        width = w;
     }
 
     public Rectangle(double side) {
-        this.length = side;
-        this.width = side;
+        length = side;
+        width = side;
     }
 
     public double calculateArea() {
         return length * width;
     }
 }
-
-class Circle extends Shape {
-    private double radius;
-
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
-    public double calculateArea() {
-        return Math.PI * radius * radius;
-    }
-}
-
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Rectangle rect1 = new Rectangle(2.5, 3.2);
         Rectangle rect2 = new Rectangle(4.0); // Using the overloaded constructor
-        Circle cir = new Circle(5.2);
 
         System.out.println("Area of rectangle 1: " + rect1.calculateArea());
         System.out.println("Area of rectangle 2: " + rect2.calculateArea());
-        System.out.println("Area of circle: " + cir.calculateArea());
     }
 }
+
