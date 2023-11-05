41e0a663ac3010bbeb6ecc78d609ef28122409bd
khushipunjabi22 <128999136+khushipunjabi22@users.noreply.github.com>
2023-11-06 01:23:18 +0530

    Create listop.hs

---
+++ listop.hs
@@ -0,0 +1,16 @@
+let myList = [1, 2, 3, 4, 5]
+let isMember = 3 `elem` myList  -- isMember will be True
+
+let myList = [1, 2, 3, 4, 5]
+let listLength = length myList  -- listLength will be 5
+
+let myList = [1, 2, 3, 4, 5]
+let first = head myList  -- first will be 1
+let rest = tail myList  -- rest will be [2, 3, 4, 5]
+
+let myList = [1, 2, 3, 4, 5]
+let lastElem = last myList  -- lastElem will be 5
+let initList = init myList  -- initList will be [1, 2, 3, 4]
+
+let myList = [1, 2, 3, 4, 5]
+let reversedList = reverse myList  -- reversedList will be [5, 4, 3, 2, 1]
