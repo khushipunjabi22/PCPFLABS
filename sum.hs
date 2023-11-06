2b35e48a3cd61e4c5b04501595a62077e837bac6
khushipunjabi22 <128999136+khushipunjabi22@users.noreply.github.com>
2023-11-06 23:56:16 +0530

    Update sum.hs

--- sum.hs
+++ sum.hs
@@ -6,3 +6,16 @@ main = do
     let myList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     let result = sumOddNumbers myList
     putStrLn ("Sum of odd numbers: " ++ show result)
+
+productOfOddNumbers :: [Int] -> Int
+productOfOddNumbers [] = 1  -- Base case: empty list has a product of 1
+productOfOddNumbers (x:xs)
+    | odd x     = x * productOfOddNumbers xs  -- Multiply odd numbers
+    | otherwise = productOfOddNumbers xs      -- Skip even numbers
+
+main :: IO ()
+main = do
+    let myList = [1, 2, 3, 4, 5]
+    let result = productOfOddNumbers myList
+    putStrLn ("Product of odd numbers: " ++ show result)
+
