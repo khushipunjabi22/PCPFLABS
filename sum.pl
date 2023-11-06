13f707a993839b3bec1ac5964f7af079edc2cc0c
khushipunjabi22 <128999136+khushipunjabi22@users.noreply.github.com>
2023-11-06 19:41:39 +0530

    Update sum.pl

--- sum.pl
+++ sum.pl
@@ -4,6 +4,25 @@ sum_list([X|Xs], Sum) :-  % Recursive case: Sum of the list = X + Sum of the res
     Sum is X + Rest.
 
 
+sum_list([], 0).
+sum_list([X|Xs], Sum) :-
+    X > 0,
+    sum_list(Xs, Rest),
+    Sum is X + Rest.
+sum_list([X|Xs], Sum) :-
+    X <= 0,
+    sum_list(Xs, Sum).
+
 
 sum_elements(List, Sum) :-
     sum_list(List, Sum).
+
+
+sum_odd_numbers([], 0).
+sum_odd_numbers([X|Xs], Sum) :-
+    X mod 2 =:= 1, 
+    sum_odd_numbers(Xs, Rest),
+    Sum is X + Rest.
+sum_odd_numbers([X|Xs], Sum) :-
+    X mod 2 =:= 0,
+    sum_odd_numbers(Xs, Sum).
