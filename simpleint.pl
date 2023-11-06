78262183fc8f7e9c685ef002b6678d5832fd9c55
khushipunjabi22 <128999136+khushipunjabi22@users.noreply.github.com>
2023-11-07 01:22:28 +0530

    Update simpleint.pl

--- simpleint.pl
+++ simpleint.pl
@@ -1,15 +1,18 @@
-
-total_amount(P, R, T, A):-
-A is P + (P * R * T).
+% Predicate to calculate total amount with simple interest
+total_amount(P, R, T, A) :-
+    A is P + (P * R * T).
 
 % Predicate to calculate principal amount with simple interest
-principal_amount(T, R, T, P) :- P is T / (1 + R * T).
+principal_amount(T, R, I, P) :-
+    P is I / (R * T).
 
 % Predicate to calculate time in years with simple interest
-time_in_years(P, T, R, T) :- T is (T - P) / (P * R).
+time_in_years(P, R, I, T) :-
+    T is I / (P * R).
 
 % Predicate to calculate simple interest
-simple_interest(P, R, T, I) :- I is P * R * T.
+simple_interest(P, R, T, I) :-
+    I is P * R * T.
 
 % Predicate to calculate compound interest
 compound_interest(P, R, T, N, CI) :-
